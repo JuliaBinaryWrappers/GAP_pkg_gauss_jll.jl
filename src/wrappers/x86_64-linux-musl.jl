7f3f46369d528137626b790d1799fe96a5766bc1
jlbuild <juliabuildbot@gmail.com>
2025-06-17 15:59:23 +0000

    GAP_pkg_gauss_jll build 202400.1100.101+0

--- src/wrappers/x86_64-linux-musl.jl
+++ src/wrappers/x86_64-linux-musl.jl
@@ -2,11 +2,10 @@
 export gauss
 
 using GAP_jll
-using GAP_lib_jll
 JLLWrappers.@generate_wrapper_header("GAP_pkg_gauss")
 JLLWrappers.@declare_file_product(gauss)
 function __init__()
-    JLLWrappers.@generate_init_header(GAP_jll, GAP_lib_jll)
+    JLLWrappers.@generate_init_header(GAP_jll)
     JLLWrappers.@init_file_product(
         gauss,
         "lib/gap/gauss.so",
